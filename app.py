39f77381dc162fecdae6764ee5f5177a0a75f5b4
moadams847 <moadams847@gmail.com>
2023-10-12 15:02:45 +0000

    initial commit

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from PIL import Image
 st.header('🎈 R x Python Streamlit App')
 # Specify the path to the Rscript executable
 
-process1 = subprocess.Popen(["Rscript", "helloworld.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-result1 = process1.communicate()
-# print(result1)
-st.write(result1)
+# process1 = subprocess.Popen(["Rscript", "helloworld.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+# result1 = process1.communicate()
+# # print(result1)
+# st.write(result1)
 
-st.subheader('2. Creating a plot using `ggplot2`')
+st.subheader('Creating a plot using `ggplot2`')
 
 process2 = subprocess.Popen(["Rscript", "plot.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result2 = process2.communicate()
